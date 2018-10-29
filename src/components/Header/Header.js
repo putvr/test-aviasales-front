2869ca16c95c2335d2a2d33d625afc5fa2f77b9a
krycio <putilovroman@gmail.com>
2018-10-30 03:43:26 +0500

    #task1 Initial

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src={logo} alt=""/>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
