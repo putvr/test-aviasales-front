fb433ea20a501c18242adf4f1763086d68157f16
krycio <putilovroman@gmail.com>
2018-10-30 15:45:03 +0500

    #task1 Ticket fixes

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -10,6 +10,29 @@ class Content extends Component {
         }
     }
 
+    getDayOfWeek(string) {
+
+        const days = ['Пн',
+            'Вт',
+            'Ср',
+            'Чт',
+            'Пт',
+            'Сб',
+            'Вс'];
+
+        let date = new Date();
+
+        date.setFullYear(
+            '20' + string.split('.')[2],
+            string.split('.')[1] - 1,
+            string.split('.')[0]);
+
+        console.log(date, string);
+
+        return days[date.getDay() - 1];
+
+    }
+
     componentWillMount() {
         let data = [{
             "origin": "VVO",
@@ -35,12 +58,135 @@ class Content extends Component {
             "carrier": "S7",
             "stops": 1,
             "price": 13100
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "12:10",
+            "arrival_date": "12.05.18",
+            "arrival_time": "18:10",
+            "carrier": "SU",
+            "stops": 0,
+            "price": 15300
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "17:00",
+            "arrival_date": "12.05.18",
+            "arrival_time": "23:30",
+            "carrier": "TK",
+            "stops": 2,
+            "price": 11000
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "12:10",
+            "arrival_date": "12.05.18",
+            "arrival_time": "20:15",
+            "carrier": "BA",
+            "stops": 3,
+            "price": 13400
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "9:40",
+            "arrival_date": "12.05.18",
+            "arrival_time": "19:25",
+            "carrier": "SU",
+            "stops": 3,
+            "price": 12450
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "17:10",
+            "arrival_date": "12.05.18",
+            "arrival_time": "23:45",
+            "carrier": "TK",
+            "stops": 1,
+            "price": 13600
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "6:10",
+            "arrival_date": "12.05.18",
+            "arrival_time": "15:25",
+            "carrier": "TK",
+            "stops": 0,
+            "price": 14250
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "16:50",
+            "arrival_date": "12.05.18",
+            "arrival_time": "23:35",
+            "carrier": "SU",
+            "stops": 1,
+            "price": 16700
+        }, {
+            "origin": "VVO",
+            "origin_name": "Владивосток",
+            "destination": "TLV",
+            "destination_name": "Тель-Авив",
+            "departure_date": "12.05.18",
+            "departure_time": "6:10",
+            "arrival_date": "12.05.18",
+            "arrival_time": "16:15",
+            "carrier": "S7",
+            "stops": 0,
+            "price": 17400
         }];
+
+        data.map((value) => {
+
+            let stops_attr;
+            let stops_count;
+
+            switch (value.stops) {
+                case 0:
+                    stops_attr = '';
+                    stops_count = '';
+                    break;
+                case 1:
+                    stops_attr = 'пересадка';
+                    stops_count = 1;
+                    break;
+                default:
+                    stops_attr = 'пересадки';
+                    stops_count = value.stops;
+            }
+
+            return (
+                value.stops_attr = stops_attr,
+                    value.stops_count = stops_count,
+                    value.departure_date_day = this.getDayOfWeek(value.departure_date),
+                    value.arrival_date_day = this.getDayOfWeek(value.arrival_date)
+            )
+        });
+
         this.setState({
             'tickets': data
         });
     }
-
 
     render() {
         return (
--- src/components/Content/Ticket/Ticket.js
+++ src/components/Content/Ticket/Ticket.js
@@ -16,22 +16,23 @@ const Ticket = (props) => (
         <div className="TicketSchedule">
 
             <div className="TicketLine-1">
-                <div className="TicketTime">
-                    {props.ticket.departure_time}<br/>
-
+                <div className="TicketDeparture FontTime">{props.ticket.departure_time}</div>
+                <div className="TicketStops">
+                    {props.ticket.stops_count} <br/>
+                    {props.ticket.stops_attr}
                 </div>
-                <div className="TicketStops">{props.ticket.stops}</div>
-                <div className="TicketTime">{props.ticket.arrival_time}</div>
+                <div className="TicketArrival FontTime">{props.ticket.arrival_time}</div>
             </div>
 
-            <div className="TicketLine-2">
-                <div>{props.ticket.origin}, {props.ticket.origin_name}</div>
-                <div>{props.ticket.destination}, {props.ticket.destination_name}</div>
-            </div>
-
-            <div className="TicketLine-2">
-                <div>{props.ticket.departure_date}, {}</div>
-                <div>{props.ticket.arrival_date}, {}</div>
+            <div className="TicketLine-2 FontCity">
+                <div className="TicketDeparture">
+                    {props.ticket.origin}, {props.ticket.origin_name} <br/>
+                    {props.ticket.departure_date}, {props.ticket.departure_date_day}
+                </div>
+                <div className="TicketArrival ">
+                    {props.ticket.destination}, {props.ticket.destination_name} <br/>
+                    {props.ticket.arrival_date}, {props.ticket.arrival_date_day}
+                </div>
             </div>
         </div>
     </div>
