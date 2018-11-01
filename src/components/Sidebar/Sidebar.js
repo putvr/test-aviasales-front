b81ca6607c5cafd700260947692d0976187dd807
krycio <putilovroman@gmail.com>
2018-11-01 11:43:59 +0500

    #task2 Sidebar fixes

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,18 +9,16 @@ const Sidebar = (props) => (
     <div className="col-md-3 ">
         <div className="container rounded border-white Sidebar">
             <div className="raw no-gutters pt-3">
-                <div className="col">
+                <div className="col pb-2">
                     <span>ВАЛЮТА</span>
                 </div>
-                <div className="col pb-2"></div>
                 <div className="col">
                     <CurrencyFilter
 
                     />
                 </div>
             </div>
-            <div className="raw pt-3"></div>
-            <div className="raw no-gutters">
+            <div className="raw no-gutters pt-3 pb-3">
                 <div className="col">
                     <span>КОЛИЧЕСТВО ПЕРЕСАДОК</span>
                 </div>
@@ -31,7 +29,6 @@ const Sidebar = (props) => (
                     />
                 </div>
             </div>
-            <div className="raw mb-3 pb-3"></div>
         </div>
     </div>
 )
