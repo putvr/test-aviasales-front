be6b305011d8d4de9ae0813f7a38c1cee1799b74
putvr <putilovroman@gmail.com>
2019-12-31 12:18:07 +0500

    Some reformatting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,7 @@ import logo from './logo.svg';
 const Header = () => {
     return (
         <header className="col text-center">
-            <div>
-                <img src={logo} alt=""/>
-            </div>
+            <img src={logo} alt=""/>
         </header>
     );
 };
